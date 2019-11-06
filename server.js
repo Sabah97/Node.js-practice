bd3ba36702f2dcb8150702104dad2731cfa92f02
Sabah97 <sabah.ashraf97@gmail.com>
2019-11-06 08:55:20 +0600

    8b

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 var http = require('http');
-var fs=require ('fs')
-var server = http.createServer(function(req, res){
-  
-  fs.readFile('index.html', function(err,data){
-      if(err){
+var express=require('express');
+var app= express();
+var server =http.Server(app);
 
-          res.statusCode =404;
-          res.setHeader('Content-Type', 'text/plain');
-            res.end('File not found')
-      }
+app.get('/', function(request,response){
+  response.sendFile(__dirname + '/index.html')
+})
+app.get('/second', function(request,response){
+  response.sendFile(__dirname + '/node.html')
+})
+// var fs=require ('fs')
+// var server = http.createServer(function(req, res){
+  
+//   fs.readFile('index.html', function(err,data){
+//       if(err){
+
+//           res.statusCode =404;
+//           res.setHeader('Content-Type', 'text/plain');
+//             res.end('File not found')
+//       }
       
-      res.setHeader('Content-Type', 'text/html');
-      res.statusCode= 200;
-      res.end(data);
-  })
-//   res.end("Hello World\n");
-});
+//       res.setHeader('Content-Type', 'text/html');
+//       res.statusCode= 200;
+//       res.end(data);
+//   })
+// //   res.end("Hello World\n");
+// });
 server.listen(3000, 'localhost', function(){
   console.log('Server running');
 });
