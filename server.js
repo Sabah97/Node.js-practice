cd43ef7cf6ca49894d5d949601ad638edd8bc606
Sabah97 <sabah.ashraf97@gmail.com>
2019-11-06 09:28:37 +0600

    8c

--- server.js
+++ server.js
@@ -2,12 +2,23 @@ var http = require('http');
 var express=require('express');
 var app= express();
 var server =http.Server(app);
+var bodyParser=require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.post('/new_article', function(request,response){
+  console.log(request.body)
+  response.json({msg:"Successfully received"})
+})
 
 app.get('/', function(request,response){
   response.sendFile(__dirname + '/index.html')
 })
 app.get('/second', function(request,response){
   response.sendFile(__dirname + '/node.html')
+})
+
+app.get('/new', function(request,response){
+  response.sendFile(__dirname + '/form.html')
 })
 // var fs=require ('fs')
 // var server = http.createServer(function(req, res){
